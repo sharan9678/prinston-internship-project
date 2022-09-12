5fe15ed9190a8ee53679f1b6fea168a08b54de00
sharan9678 <sharan9678@gmail.com>
2022-09-12 16:55:12 +0530

    added endpoints for authentication and user validation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 // initialize the express
 const app = express();
@@ -29,9 +30,10 @@ mongoose.connection.on("conneted", () => {
     console.log("mongodb connected!");
 });
 
-app.use(express.json());
-
 // middlewares
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
@@ -41,14 +43,14 @@ app.use("/api/rooms", roomsRoute);
 // middleware to handle errors
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
-    const errorMessage = err.message || "something went wrong!";
+    const errorMessage = err.message || "Something went wrong!";
     return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
     });
-})
+});
 
 app.listen(8080, () => {
     connect();
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,31 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("hello user, yor are logged in!");
+// });
 
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("hello user, yor are logged in and you can delete your account!");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("hello admin, yor are logged in and you can delete all accounts!");
+// });
+
+// update
+router.put("/:id", verifyUser, updateUser);
+
+// delete
+router.delete("/:id", verifyUser, deleteUser);
+
+// get user by id
+router.get("/:id", verifyUser, getUser);
+
+// get all users
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
