62265240bd317f63c33d7893b98d2af9cf74bc0b
sharan9678 <sharan9678@gmail.com>
2022-09-20 16:19:01 +0530

    added search context and modified list and hotel pages

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useFetch = (url) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const res = await axios.get(url);
-                setData(res.data);
-            } catch (error) {
-                setError(true);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    }, [url]);
-
-    const reFetch = async () => {
-        setLoading(true);
-        try {
-            const res = await axios.get(url);
-            setData(res.data);
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
     };
+    fetchData();
+  }, [url]);
 
-    return {data, loading, error, reFetch};
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
 };
 
 export default useFetch;
